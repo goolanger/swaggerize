18f366a5b73135e80b14f79810955e21861b24eb
amaury95 <amauryuh@gmail.com>
2020-08-13 15:46:56 -0400

    fixed gorm datetime mess.

--- models/model/gorm/datetime.go
+++ models/model/gorm/datetime.go
@@ -5,27 +5,8 @@ import (
 	"github.com/goolanger/swaggerize/models/swagger"
 )
 
-type datetime struct {
-
-}
-
-func (d *datetime) GetName() string {
-	panic("implement me")
-}
-
-func (d *datetime) GetRep() map[string]interface{} {
-	return map[string]interface{}{
-		"format": "date-time",
-		"type": "string",
-	}
-}
-
-func (d *datetime) GetRef() swagger.Definition {
-	return d
-}
-
 func DateTime(name, description string) swagger.Definition {
-	return model.Property(name, &datetime{}).
+	return model.Property(name, model.DateTime()).
 		Tag("x-go-custom-tag", "gorm:\"Type:timestamp\"").
 		Description(description)
 }
